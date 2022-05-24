f8fa08b0778101ca32f99f985942ced261182083
liria <fdshu@uwaterloo.ca>
2022-05-24 11:01:26 -0400

    Fix strike

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -51,7 +51,7 @@ export default function Bio() {
           alt="Profile"
         />
         <p className="mb-0 lg:text-lg text-gray-800">
-          Hi, I'm a ~~math student~~ automation developer, a 3d printing nerd, and a traditional archery lover.
+          Hi, I'm a <strike>math student</strike> automation developer, a 3d printing nerd, and a traditional archery lover.
         </p>
       </div>
       <div className="mb-8">
